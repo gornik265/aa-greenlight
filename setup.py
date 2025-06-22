067a8bae4709b54b35168ce2a93dd4f9d18a3695
gornik265 <piotrekcz94@gmail.com>
2025-06-22 14:02:40 +0200

    Finishing up admin panel

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name='aa-greenlight',
     version='0.1.0',
+    description='AllianceAuth plugin for fleet pings via Discord',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/gornik265/aa-greenlight',
+    author='gornik265',
+    author_email='piotrekcz94@gmail.com.com',
+    license='MIT',
     packages=find_packages(),
     include_package_data=True,
-    license='MIT',
-    description='AllianceAuth plugin for Discord alert buttons',
-    long_description=open('README.md').read(),
-    url='https://github.com/YOUR_USERNAME/aa-greenlight',
-    author='Your Name',
-    author_email='your@email.com',
+    install_requires=[
+        'requests',
+    ],
     classifiers=[
         'Framework :: Django',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
     ],
-    install_requires=['requests'],
 )
